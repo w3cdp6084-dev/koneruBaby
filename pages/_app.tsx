0bf5b753a4aa0521933fbd087cf40a7cb09fc4b6
w3cdp6084-dev <yusukew3@gmail.com>
2022-12-24 08:47:56 +0900

    Footer progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.scss'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 function MyApp({ Component, pageProps }) {
   return(
-  <>
-    <Header />
-    <Component {...pageProps} />
-  </>
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
   )
 }
 
