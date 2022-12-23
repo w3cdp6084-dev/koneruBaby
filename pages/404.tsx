4bc00cf403d6db27878394fc6dc7b0ba71f5fd9d
w3cdp6084-dev <yusukew3@gmail.com>
2022-12-24 08:12:16 +0900

    404 styling progress

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/404.module.scss'
 export default function Custom404() {
     return (
-      <main className="main">
-        <p>ページがありません。</p>
+      <main className={styles.main}>
+        <p className={styles.title}>404</p>
+        <p className={styles.description}>ページがありません🐱🐱🐱</p>
       </main>
     );
   }
