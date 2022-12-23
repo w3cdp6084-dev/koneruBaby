2a89966c4ebd6199eb2013530bcd979e0f93f534
w3cdp6084-dev <yusukew3@gmail.com>
2022-12-24 07:03:12 +0900

    microcms set

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { client } from "../libs/client";
+
+export default function Home({ blog }) {
+  return (
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              {blog.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
