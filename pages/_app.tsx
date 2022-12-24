30c55df7329d8a3c7a071dff4960417e281aef7b
w3cdp6084-dev <yusukew3@gmail.com>
2022-12-24 12:11:25 +0900

    reset css add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/reset.css'
 import '../styles/globals.scss'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
+import styles from "../styles/Card.module.scss";
 import Link from "next/link";
 import { client } from "../libs/client";
 
 export default function Home({ blog }) {
   return (
     <div>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>
-              {blog.title}
-            </Link>
-            <div>{blog.category.name}</div>
-          </li>
-        ))}
-      </ul>
-      
+      <div className={styles.wrap}>
+        <ul className={styles.card}>
+          {blog.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>
+                {blog.title}
+              </Link>
+              <div>{blog.category.name}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
