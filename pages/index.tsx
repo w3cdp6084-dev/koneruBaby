c9d344a5131669f7172bede7d32c748df168ae66
w3cdp6084-dev <yusukew3@gmail.com>
2022-12-24 10:38:28 +0900

    Card delete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
-
-import Card from "../components/Card";
+import Link from "next/link";
+import { client } from "../libs/client";
 
 export default function Home({ blog }) {
   return (
     <div>
-      <Card blog={blog} />
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              {blog.title}
+            </Link>
+            <div>{blog.category.name}</div>
+          </li>
+        ))}
+      </ul>
+      
     </div>
   );
 }
+
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
